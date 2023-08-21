0d786e22c9319545ac2fce638ec3949372bb284d
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-08-21 20:16:50 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -5,6 +5,7 @@ public class Main {
     static int N,T,P,result;
     static int[][] arr;
     static boolean[] visit;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bw=  new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,6 +16,7 @@ public class Main {
         P = 0;
         result = 0;
         arr = new int[N][2];
+        dp = new int[N];
         visit = new boolean[N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,21 +25,20 @@ public class Main {
             arr[i][0] = t;
             arr[i][1] = p;
         }
-        recur(0,0);
-        bw.write(result+"");
+        int recur = recur(0);
+        bw.write(recur+"");
         bw.flush();
 
     }
-    static void recur(int day,int price){
+    static int recur(int day){
         if(day == N) {
-            if (price > result){
-                result = price;
-                return;
+                return 0;
             }
-            return;
+        if(day > N) return -999999999;
+        if (dp[day] != 0){
+            return dp[day];
         }
-        if(day > N) return;
-        recur(day+ arr[day][0],price +arr[day][1]);
-        recur(day+1,price);
+        dp[day] = Math.max(recur(day + arr[day][0])+ arr[day][1], recur(day + 1));
+        return dp[day];
     }
 }
