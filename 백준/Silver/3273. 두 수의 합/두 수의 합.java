1b34f703694e89df2f68da15a4313dd920afbfff
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-08-30 14:38:40 +0900

    [Silver III] Title: 두 수의 합, Time: 372 ms, Memory: 26252 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -4,29 +4,35 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw=  new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int[] an = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int ai = Integer.parseInt(st.nextToken());
-            an[i]  = ai;
+        int[] an = new int[n];
+        for (int i = 0; i < an.length; i++) {
+            an[i] = Integer.parseInt(st.nextToken());
         }
+        int[] sortedAn = Arrays.stream(an).sorted().toArray();
         int x = Integer.parseInt(br.readLine());
-        int[] sortAn = Arrays.stream(an).sorted().toArray();
-
-        int s = 0;
-        int e = n - 1;
+        int start = 0;
+        int end = n - 1;
         int count = 0;
-        while(s < e){
-            //정답조건
-            if(sortAn[s] + sortAn[e] == x) count++;
-            if(sortAn[s] + sortAn[e] >= x) e--;
-            else s++;
+        while(start != end){
+            if(sortedAn[start] + sortedAn[end] == x){
+                count++;
+                start++;
+                continue;
+            }
+            if(sortedAn[start] + sortedAn[end] >x){
+                end--;
+                continue;
+            }
+            if(sortedAn[start] + sortedAn[end] < x) {
+                start++;
+            }
         }
-        bw.write(count+"");
+        bw.write(String.valueOf(count));
         bw.flush();
     }
+
 }
