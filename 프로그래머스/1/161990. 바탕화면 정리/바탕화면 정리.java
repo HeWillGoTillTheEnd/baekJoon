8f2da558bf6d8f81ea943b7e389bc57f7a8d318c
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-11-01 17:34:27 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = 50;
+        int luy = 50;
+        int rdx = 0;
+        int rdy = 0;
+
+        for(int i = 0; i< wallpaper.length;i++){
+            for(int j = 0; j< wallpaper[i].length();j++){
+                if(wallpaper[i].charAt(j) == '#' ){
+                    if(lux > j) lux = j;
+                    if(luy > i) luy = i;
+                    if(rdx < j) rdx = j;
+                    if(rdy < i) rdy = i;
+                }
+            }
+        }
+        int[] answer = {luy, lux, rdy+1, rdx+1};
+        return answer;
+    }
+}
