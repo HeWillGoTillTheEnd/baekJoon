93de50a37cbdc3624e15e1a9abc6afc3f36f48e2
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2024-10-22 23:09:14 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,29 +1,27 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[][] intArr = new int[9][9];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
         int max = 0;
-        int saveI = 0;
-        int saveJ = 0;
-        for(int i = 0;i<9;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0;j<9;j++){
-                intArr[i][j] = Integer.parseInt(st.nextToken());
-                if(i == 0&& j ==0) {
-                    max = intArr[i][j];
-                }
-                if(max<=intArr[i][j]) {
-                    max = intArr[i][j];
-                    saveI = i+1;
-                    saveJ = j+1;
+        int hang = 1;
+        int yeol = 1;
+        for(int i =1;i<=9;i++){
+            String s = br.readLine();
+            String[] split = s.split(" ");
+            for(int j = 1;j<=9;j++){
+                int i1 = Integer.parseInt(split[j-1]);
+                if(i1 > max) {
+                    max = i1;
+                    hang = i;
+                    yeol = j;
                 }
             }
         }
-        bw.write(max+"\n"+saveI+" "+saveJ);
+        bw.write(max + "\n");
+        bw.write(hang + " " + yeol);
         bw.flush();
     }
 }
