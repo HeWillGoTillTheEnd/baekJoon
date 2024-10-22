36915e85044bb54eb74b52fda59dd09748ddc224
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2024-10-22 22:40:23 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 180 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,25 +1,38 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] a = new int[N][M];
-        for(int k = 0;k<2;k++) {
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < M; j++) {
-                    a[i][j] += Integer.parseInt(st.nextToken());
-                }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+        String s = br.readLine();
+        String[] split = s.split(" ");
+        int N = Integer.parseInt(split[0]);
+        int M = Integer.parseInt(split[1]);
+        int[][] ints = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            String s1 = br.readLine();
+            String[] split1 = s1.split(" ");
+            int[] ints1 = new int[M];
+            for(int j = 0; j < ints1.length; j++){
+                ints1[j] = Integer.parseInt(split1[j]);
             }
+            ints[i] = ints1;
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                bw.write(String.valueOf(a[i][j] + " "));
+        int[][] ints2 = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            String s1 = br.readLine();
+            String[] split1 = s1.split(" ");
+            int[] ints1 = new int[M];
+            for(int j = 0; j < ints1.length; j++){
+                ints1[j] = Integer.parseInt(split1[j]);
+            }
+            ints2[i] = ints1;
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0;j< M; j++){
+                bw.write((ints[i][j] + ints2[i][j])+ " ");
             }
             bw.write("\n");
         }
